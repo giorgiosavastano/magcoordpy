d8b8c277ac4daaa65df054f93ce96adab4e9aa72
Giorgio Savastano <giorgiosavastano@gmail.com>
2022-03-10 11:34:02 +0100

    improving sphinx docs (#15)
    Co-authored-by: Giorgio Savastano <giorgio.savastano@uniroma1.it>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import sys
 from typing import List
 
 sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("../../src/magcoordpy"))
 
 
